d03bec82a1cedce1ea8782405452b246cf58af89
Sammy Senior <sammynz@gmail.com>
2023-08-21 17:34:04 +1200

    Built the navbar component

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_CSS_MANIFEST={"__entry_css__":{"/Users/sammysenior/Documents/CODE/2023/github repos/metaversus/app/page":["/Users/sammysenior/Documents/CODE/2023/github repos/metaversus/styles/globals.css"]},"/Users/sammysenior/Documents/CODE/2023/github repos/metaversus/app/layout.js":["/Users/sammysenior/Documents/CODE/2023/github repos/metaversus/styles/globals.css"]}
